e5d6d388efad45dbfaa474649f29a5513a56d7ba
Mohamed <mde@frianbiz.com>
2020-02-17 20:27:44 +0100

    Initial Commit

---
+++ IntruPod/IntruPod.h
@@ -0,0 +1,19 @@
+//
+//  IntruPod.h
+//  IntruPod
+//
+//  Created by Mohamed Derkaoui on 17/02/2020.
+//  Copyright © 2020 Mohamed Derkaoui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IntruPod.
+FOUNDATION_EXPORT double IntruPodVersionNumber;
+
+//! Project version string for IntruPod.
+FOUNDATION_EXPORT const unsigned char IntruPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IntruPod/PublicHeader.h>
+
+
